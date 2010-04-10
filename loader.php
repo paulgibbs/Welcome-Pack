96dd71f297489a6f70fc1026465856670633ceb2
Paul Gibbs <djpaul@gmail.com>
2010-04-10 12:35:26 +0000

    Updated translation for meta desc

--- loader.php
+++ loader.php
@@ -32,6 +32,7 @@ along with this program.  If not, see http://www.gnu.org/licenses/.
 
 
 function dpw_init() {
+	__( 'When a user registers on your site, Welcome Pack lets you automatically send them a friend or group invitation, or a welcome message. You can also customise the default emails sent by BuddyPress to ensure that they match the brand and tone of your site.', 'dpw' );  // Metadata description translation
 	require( dirname( __FILE__ ) . '/includes/welcome-pack-core.php' );
 
 	if ( !get_site_option( 'welcomepack' ) )

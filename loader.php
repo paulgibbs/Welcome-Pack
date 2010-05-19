efd89df0a76d4ad59be6e97233f7247447001de5
Paul Gibbs <djpaul@gmail.com>
2010-05-19 21:34:33 +0000

    In error.

--- loader.php
+++ loader.php
@@ -32,9 +32,6 @@ along with this program.  If not, see http://www.gnu.org/licenses/.
 
 
 function dpw_init() {
-	$current_user = wp_get_current_user();
-	$current_user->has_cap('something');
-
 	__( 'When a user registers on your site, Welcome Pack lets you automatically send them a friend or group invitation, or a welcome message. You can also customise the default emails sent by BuddyPress to ensure that they match the brand and tone of your site.', 'dpw' );  // Metadata description translation
 	require( dirname( __FILE__ ) . '/includes/welcome-pack-core.php' );
 

1b55e2797c2a428e79fbdb655db373b1f3f2b6d2
Paul Gibbs <djpaul@gmail.com>
2011-01-15 22:27:43 +0000

    Remove unneeded javascript which was used for old email implementation.

--- includes/js/admin.js
+++ includes/js/admin.js
@@ -7,7 +7,7 @@ jQuery(document).ready( function() {
 	else
 		postboxes.add_postbox_toggles('buddypress_page_welcome-pack-emails');
 
-	jQuery('div.initially-hidden').each( function() { 
+	jQuery('div.initially-hidden').each( function() {
 		jQuery(this).hide();
 	});
 
@@ -19,28 +19,6 @@ jQuery(document).ready( function() {
 			config.css('background-color', 'rgb(255,255,224)').slideDown('fast').animate( { backgroundColor: 'rgb(255,255,255)' }, 1600);
 		else
 			config.stop(true).slideUp();
-	});
-
-	jQuery('#emailpicker').change( function() {
-		var index = this.selectedIndex;
-		if ( 0 == index ) {
-			jQuery('#email').hide().empty();
-			return;
-		}
-
-		jQuery.post( ajaxurl, {
-			action: 'dpw_fetch_email',
-			'cookie': encodeURIComponent(document.cookie),
-			'_wpnonce': jQuery("#_ajax_nonce_dpw_emails").val(),
-			'id': index
-		},
-		function(response)
-		{
-			if ( response[0] + response[1] == '-1' )
-				return;
-
-			jQuery('#email').html(response.substr(0, response.length-1)).show();
-		});
 	});
 
 });

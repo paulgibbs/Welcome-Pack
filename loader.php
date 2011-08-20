34f81727211d4e7287967e95367495cddc827de2
Paul Gibbs <djpaul@gmail.com>
2011-08-20 15:34:57 +0000

    Tweak copyright notices and stuff. Move languages folder up a level.

--- loader.php
+++ loader.php
@@ -1,43 +1,39 @@
 <?php
 /**
- * Main loader file; ensures that BuddyPress is loaded first.
- *
- * @author Paul Gibbs <paul@byotos.com>
- * @package Welcome Pack
- * @subpackage loader
- *
- * $Id$
+ * Plugin Name: Welcome Pack
+ * Plugin URI: http://buddypress.org/community/groups/welcome-pack/
+ * Description: Automatically send friend/group invites and a welcome message to new users, and redirect them to a custom page. Also provides email customisation options.
+ * Version: 3.0
+ * Requires at least: WordPress 3.2, BuddyPress 1.5
+ * Tested up to: WP 3.2, BuddyPress 1.5
+ * License: GPL3
+ * Author: Paul Gibbs
+ * Author URI: http://byotos.com/
+ * Network: true
+ * Domain Path: /includes/languages/
+ * Text Domain: dpw
  */
 
-/*
-Plugin Name: Welcome Pack
-Plugin URI: http://buddypress.org/community/groups/welcome-pack/
-Description: When a user registers on your site, Welcome Pack lets you automatically send them a friend or group invitation, a Welcome Message and can redirect them to a Start Page. You can also customise the default emails sent by BuddyPress to ensure that they match the brand and tone of your site.
-Version: 2.3
-Requires at least: WordPress 3.1, BuddyPress 1.3
-Tested up to: WP 3.1, BuddyPress 1.3
-License: General Public License version 2
-Author: Paul Gibbs
-Author URI: http://byotos.com/
-Network: true
-Domain Path: /includes/languages/
-Text Domain: dpw
-
-"Welcome Pack" for BuddyPress
-Copyright (C) 2009-11 Paul Gibbs
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License version 2 as published by
-the Free Software Foundation.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see http://www.gnu.org/licenses/.
-*/
+/**
+ * Automatically send friend/group invites and a welcome message to new users, and redirect them to a custom page. Also provides email customisation options.
+ *
+ * "Welcome Pack"
+ * Copyright (C) 2009-11 Paul Gibbs
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as published by
+ * the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses/.
+ *
+ * @package Welcome Pack
+ */
 
 /**
  * Only load the component if BuddyPress is loaded and initialised.

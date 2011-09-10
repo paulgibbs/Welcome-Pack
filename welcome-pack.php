eabecde3e423be5f1219025e2183c2456c063b05
Paul Gibbs <djpaul@gmail.com>
2011-09-10 18:25:46 +0000

    Move email post type to main file. Remove old core file; adios!

--- welcome-pack.php
+++ welcome-pack.php
@@ -98,12 +98,54 @@ class DP_Welcome_Pack {
 		add_action( bp_core_admin_hook(), array( $this, 'setup_admin_menu' ) );
 		add_filter( 'plugin_action_links', array( $this, 'add_settings_link' ), 10, 2 );
 
+		// Emails
+		add_action( 'bp_init', array( $this, 'register_post_types' ) );
+
 		// Start page
 		add_filter( 'login_redirect', array( $this, 'redirect_login' ), 20, 3 );
 		add_filter( 'ws_plugin__s2member_fill_login_redirect_rc_vars', array( $this, 'redirect_s2member_login' ), 10, 2 );
 
 		// Things that happen when a user's account is activated
 		add_action( 'bp_core_activated_user', array( $this, 'user_activated' ) );
+	}
+
+	/**
+	 * Register email post type
+	 *
+	 * @since 3.0
+	 */
+	function register_post_types() {
+		$email_labels = array(
+			'name'               => __( 'Emails',                   'dpw' ),
+			'singular_name'      => __( 'Email',                    'dpw' ),
+			'add_new'            => __( 'New email',                'dpw' ),
+			'add_new_item'       => __( 'Create new email',         'dpw' ),
+			'edit'               => __( 'Edit',                     'dpw' ),
+			'edit_item'          => __( 'Edit email',               'dpw' ),
+			'new_item'           => __( 'New email',                'dpw' ),
+			'view'               => __( 'View email',               'dpw' ),
+			'view_item'          => __( 'View email',               'dpw' ),
+			'search_items'       => __( 'Search emails',            'dpw' ),
+			'not_found'          => __( 'No emails found',          'dpw' ),
+			'not_found_in_trash' => __( 'No emails found in Trash', 'dpw' )
+		);
+
+		$email_supports = array(
+			'editor',
+			'page-attributes',
+			'revisions',
+			'title'
+		);
+
+		$email_cpt = array(
+			'labels'          => $email_labels,
+			'public'          => false,
+			'show_in_menu'    => false,
+			'show_ui'         => true,
+			'supports'        => $email_supports
+		);
+
+		register_post_type( 'dpw_email', $email_cpt );
 	}
 
 	/**

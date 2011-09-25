3650a26702b6e768f42f3e795436bafd05f3946e
Paul Gibbs <djpaul@gmail.com>
2011-09-25 11:56:46 +0000

    Correct name of template file

--- templates/welcome_pack_default.php
+++ templates/welcome_pack_default.php
@@ -3,4 +3,50 @@
 Email Template: Welcome Pack Default
 */
 ?>
-<h1>yarly</h1>
+<html>
+	<body bgcolor="#f9f9f9" link="#D84800" alink="#D84800" vlink="#D84800" marginheight="0" topmargin="0" marginwidth="0" leftmargin="0" style="margin: 0px; background-color: #f9f9f9; font-family: Helvetica, Arial, sans-serif;">
+		<table cellpadding="0" cellspacing="0" width="100%" bgcolor="#f9f9f9" border="0">
+			<tr>
+				<td style="padding:15px;" align="center">
+					<center>
+						<table width="550" cellpadding="0" bgcolor="#ffffff" cellspacing="0" >
+							<tr>
+								<td>
+									<div style="border:solid 1px #d9d9d9;">
+										<table id="header" width="100%;" border="0" cellpadding="0" bgcolor="#ffffff" cellspacing="0" style="line-height:1.5;font-size:12px;font-family: Helvetica, Arial, sans-serif;border:solid 1px #FFFFFF;">
+											<tr>
+												<td colspan="2" background="%blog_url%/wp-admin/images/white-grad-active.png" height="30"> </td>
+											</tr>
+											<tr >
+												<td style="line-height:32px;padding: 0 0 0 30px;" valign="baseline">
+													<span style="font-size:32px;"><a href="%blog_url%" style="text-decoration:none;" target="_blank">%blog_name%</a></span>
+												</td>
+												<td style="padding: 0 30px 0 0;" align="right" valign="baseline">
+													<span style="font-size:14px;color:#777777">%blog_description%</span>
+												</td>
+											</tr>
+										</table>
+										<table id="content" width="100%;" border="0" cellpadding="0" bgcolor="#ffffff" cellspacing="0" style="color:#444;line-height:1.5;font-size:12px;font-family: Arial, sans-serif;padding:20px 30px 0 30px;">
+											<tr>
+												<td colspan="2" style="padding:20px 0;border-top: solid 1px #d9d9d9">%content%</td>
+											</tr>
+										</table>
+										<table id="footer" width="100%;" border="0" cellpadding="0" bgcolor="#ffffff" cellspacing="0" style="line-height:1.5;font-size:12px;font-family: Arial, sans-serif;padding:0 30px 15px 30px;">
+											<tr style="font-size:11px;color:#777777;">
+												<td style="border-top: solid 1px #d9d9d9;" colspan="2">
+													<img style="padding:15px 0 0 0" height="32" width="32" src="%blog_url%/wp-content/plugins/buddypress/bp-themes/bp-default/_inc/images/email_logo.png" align="right" alt="Powered by BuddyPress" />
+													<div style="padding:15px 0 1px 0"><img height="13" width="13" style="vertical-align: middle;" src="%blog_url%/wp-admin/images/date-button.gif" alt="Date"  /> Email sent the %date% @ %time%</div>
+													<div><img height="12" width="12" style="vertical-align: middle;" src="%blog_url%/wp-content/images/comment-grey-bubble.png" alt="Contact"  /> For any request, please contact <a href="mailto:%admin_email%">%admin_email%</a></div>
+												</td>
+											</tr>
+										</table>
+									</div>
+								</td>
+							</tr>
+						</table>
+					</center>
+				</td>
+			</tr>
+		</table>
+	</body>
+</html>
